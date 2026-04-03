2a62c2b796fe05d9e9b6bc3eb1a20cec401876b9
Paolo Cignoni <paolo.cignoni@isti.cnr.it>
2026-04-03 09:43:11 +0200

    Update document.h

--- document.h
+++ document.h
@@ -3,8 +3,8 @@
 #include "vcgmesh.h"
 #include <QObject>
 #include <QString>
-#include <vector>
 #include <memory>
+#include <vector>
 
 class Document : public QObject
 {
@@ -20,15 +20,27 @@ public:
 
     int loadMesh(const QString &filename);
     void removeMesh(int index);
+    void clearLog();
+    void writeLog(const QString &message);
 
     int meshCount() const { return static_cast<int>(m_meshes.size()); }
     MeshEntry &mesh(int i) { return *m_meshes[i]; }
     const MeshEntry &mesh(int i) const { return *m_meshes[i]; }
+    const std::vector<QString> &logMessages() const { return m_logMessages; }
+    vcg::CallBackPos *logCallback();
 
 signals:
     void meshAdded(int index);
     void meshRemoved(int index);
+    void logCleared();
+    void logMessageAdded(const QString &message);
 
 private:
+    bool handleLogCallback(int pos, const char *message);
+    static bool dispatchLogCallback(int pos, const char *message);
+
     std::vector<std::unique_ptr<MeshEntry>> m_meshes;
+    std::vector<QString> m_logMessages;
+    QString m_lastCallbackMessage;
+    int m_lastCallbackBucket = -1;
 };
